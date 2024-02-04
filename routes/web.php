f9607d49bcd7cd8deaedbf2c1786567d8db801fe
Stas Savin <gghg.vb.97@hotmail.com>
2024-02-04 20:49:41 +0300

    two commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\MyPlaceController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +16,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/posts', [PostController::class, 'index']);
 
-Route::get('/groups', function () {
-    return view('groups.groups');
+Route::get('', function () {
+	return view('groups.groups');
 });
