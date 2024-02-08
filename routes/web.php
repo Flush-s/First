49cef433b151a2e3bbe110bf80d4e9104b57433c
Stas Savin <gghg.vb.97@hotmail.com>
2024-02-08 22:15:03 +0300

    1

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+	return view('welcome');
+});
 
 Route::get('/groups', function () {
 	return view('groups.groups');
 });
+
